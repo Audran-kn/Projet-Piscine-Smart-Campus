6874aa93f2f251fcf13923a6d8ff56b2784726ca
mathlenh <150336510+mathlenh@users.noreply.github.com>
2026-05-29 21:56:22 +0200

    Update classes_database.php

--- bakend/classes_database.php
+++ bakend/classes_database.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . 'bakend/config_database.php';
+require_once __DIR__ . '/config_database.php';
 
 class Database {
     private static $instance = null;
@@ -9,9 +9,9 @@ class Database {
         try {
             $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
             $options = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,   
-                PDO::ATTR_EMULATE_PREPARES   => false,                
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
             ];
             $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
